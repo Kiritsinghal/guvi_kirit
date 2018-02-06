2ed44993d92de4f7e76f299419b3fcb0d2b65b61
Kirit <singhalkirit@gmail.com>
2018-02-06 22:05:07 +0530

    char difference

---
+++ player/char_differ.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+	char a[10],b[10];
+	int i,d=0;
+	gets(a);
+	gets(b);
+	if(strlen(a)!=strlen(b))
+	{
+		printf("Invalid string");
+		exit(0);
+	}
+	else
+	{
+		for(i=0;i<strlen(a);i++)
+		{
+			if(a[i]!=b[i])
+				d++;
+		}
+	}
+	if(d>1 || d==0)
+		printf("no");
+	else
+		printf("yes");
+}
